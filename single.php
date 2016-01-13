01f19992b6bcd27bed3e5a8fa774dc6793a18571
bacoords <bacoords@gmail.com>
2016-01-12 19:45:17 -0800

    blog single.php

--- single.php
+++ single.php
@@ -7,13 +7,25 @@
 
 <section>
 	<div class="page-content-row post-content-row">
-		<div class="frame frame-750">
-			<div class="bit-1">
+		<div class="frame frame-1500">
+			<div class="bit-75">
 				<BR><BR><BR>
-				<h1><?php the_title(); ?></h1>
-				<BR><BR><BR>
-				<?php the_content(); ?>
-				<BR><BR><BR>
+				<div class="frame-mini">
+					<div class="bit-1">
+						
+						<h1><?php the_title(); ?></h1>
+						<BR><BR><BR>
+						<?php the_content(); ?>
+						<BR><BR><BR>
+				
+					</div>
+				</div>
+			</div>
+			<div class="bit-25">
+			<div class="padding">
+				<br><br><br>
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			</div>
 			</div>
 		</div>
 	</div>
